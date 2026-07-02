ab79561045b09740a3ef766846d6763c65e37d57
ajc5 <3946534+ajc5@users.noreply.github.com>
2026-07-02 20:17:08 +0200

    Fix server-ui entrypoint curl -sf to -f

--- dockerhub/entrypoint-server-ui.sh
+++ dockerhub/entrypoint-server-ui.sh
@@ -5,7 +5,7 @@ echo "Starting Tangerine Server-UI..."
 
 # Wait for the server to be ready
 echo "Waiting for Tangerine Server..."
-until curl -sf http://server:80/login > /dev/null 2>&1; do
+until curl -f http://server:80/ > /dev/null 2>&1; do
     sleep 2
 done
 echo "Tangerine Server is ready."
